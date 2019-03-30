78d8426ac680a76b20becb713039ee7a1092f32d
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-03-30 12:05:02 +0100

    prepared statement examples added

--- src/main/java/pl/jdbc/StatementExamples.java
+++ src/main/java/pl/jdbc/StatementExamples.java
@@ -1,9 +1,7 @@
 package pl.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Random;
 
 public class StatementExamples {
 
@@ -14,7 +12,10 @@ public class StatementExamples {
                 "root", "kubistycznykot");
 //        createTable(connection);
 //        insert5newEmployees(connection);
-        dropTable(connection);
+//        insert5newEmplyees2(connection);
+//        dropTable(connection);
+//        dropLessThan(connection, 1000);
+        selectNameAndSalaryFromTable(connection);
     }
 
     static void createTable(Connection connection) throws SQLException {
@@ -26,6 +27,17 @@ public class StatementExamples {
                        " salary int, \n" +
                        " primary key (id))");
 
+    }
+
+    static void insert5newEmplyees2(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection
+        .prepareStatement("INSERT INTO employee9 (name, salary) values (?, ?)");
+        for (int i = 0; i <5 ; i++) {
+            preparedStatement.setString(1, "imie" + i);
+            preparedStatement.setInt(2, new Random().nextInt(2000));
+            preparedStatement.executeUpdate();
+
+        }
     }
 
     static void insert5newEmployees(Connection  connection) throws SQLException{
@@ -40,5 +52,28 @@ public class StatementExamples {
     static  void dropTable(Connection connection) throws SQLException{
         Statement statement = connection.createStatement();
         statement.execute("DROP TABLE  employee9");
+    }
+
+    //napisz metode, która usunie pracownikow, których zarobki są
+    //mniejsze niż argument podany w metodzie
+
+    static void dropLessThan (Connection connection, int salary) throws SQLException {
+        PreparedStatement preparedStatement = connection
+                .prepareStatement("delete from employee9 where salary < ?");
+        preparedStatement.setInt(1, salary);
+        // bez drugiego argumentu po connection to wtedy w setInt np (1, 1000 )
+        preparedStatement.executeUpdate();
+    }
+
+    static void selectNameAndSalaryFromTable(Connection connection) throws SQLException{
+        Statement statement =  connection.createStatement();
+        ResultSet resultSet = statement
+                .executeQuery("SELECT name, salary from employee9");
+
+        while (resultSet.next()){
+            String name = resultSet.getString("name");
+            int salary = resultSet.getInt(2);
+            System.out.println(name+ " "+salary);
+        }
     }
 }
